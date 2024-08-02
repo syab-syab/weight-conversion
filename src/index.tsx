201be2a12b118ab7969962b5724c4fb990050b39
syab-syab <syabsyabsyabsyab@gmail.com>
2024-08-02 11:10:17 +0900

    完了

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
+
+const analyticsPass: any = process.env.REACT_APP_GOOGLE_ANALYTICS_PASS
+
+ReactGA.initialize(analyticsPass);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
